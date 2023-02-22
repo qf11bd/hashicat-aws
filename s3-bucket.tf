079018ab21559c3a4a604a898a656ada6c6759a9
Brendan Dodds <brendan.dodds@live.com.au>
2023-02-22 03:58:44 +0000

    Fix S3 version

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3_bucket" {
   source        = "app.terraform.io/qf11bd-sandbox/s3-bucket/aws"
-  version       = "2.9.0"
+  version       = "2.7.0"
   bucket        = "db-s3-bucket"
   acl           = "private"
   bucket_prefix = var.prefix
